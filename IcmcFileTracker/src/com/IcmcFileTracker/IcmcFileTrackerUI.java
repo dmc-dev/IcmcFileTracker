f3cdefaae0d413846ea49262900c159e121fbecd
danny <dmc.developer@gmail.com>
2013-05-30 13:39:50 +0300

    check the role for naving to the view any users history view

--- IcmcFileTracker/src/com/IcmcFileTracker/IcmcFileTrackerUI.java
+++ IcmcFileTracker/src/com/IcmcFileTracker/IcmcFileTrackerUI.java
@@ -8,6 +8,7 @@ import com.IcmcFileTracker.Forms.LoginListener;
 import com.IcmcFileTracker.Views.DepartmentView;
 import com.IcmcFileTracker.Views.HomeView;
 import com.IcmcFileTracker.Views.NewUserView;
+import com.IcmcFileTracker.Views.UserActivityView;
 import com.IcmcFileTracker.helpers.DataInit;
 import com.IcmcFileTracker.model.*;
 
@@ -90,7 +91,7 @@ public class IcmcFileTrackerUI extends UI implements ViewChangeListener, LoginLi
 			return false;
 		}
 		
-		if(event.getViewName().equals(NewUserView.VIEW_NAME) || event.getViewName().equals(DepartmentView.VIEW_NAME))
+		if(event.getViewName().equals(NewUserView.VIEW_NAME) || event.getViewName().equals(DepartmentView.VIEW_NAME) || event.getViewName().equals(UserActivityView.VIEW_NAME))
 		{
 			if(!user.getRole().isRole(Role.admin)){
 				return false;
